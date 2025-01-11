6a701c439df097aae84a5e69a5a9729154569957
ZHADOW999 <babalolav9@gmail.com>
2025-01-11 14:59:11 +0100

    Update splash screen configuration and enhance layout options
    - Changed splash screen 'resizeMode' from 'cover' to 'contain' in app.json for better image display.
    - Modified RootLayout to hide the header by default in _layout.tsx.
    - Improved loading indicator styling in index.tsx and added a property to prevent zoom in WebView.
    
    These changes enhance the user interface and improve the overall user experience.

--- app/index.tsx
+++ app/index.tsx
@@ -3,11 +3,12 @@ import { ActivityIndicator, StyleSheet, View } from 'react-native';
 import { WebView } from 'react-native-webview';
 
 const App = () => {
+  
   const [isLoading, setIsLoading] = React.useState(true);
   return (
     <View className='flex-1'>
       {isLoading && (
-        <View className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-[#ffc107]">
+        <View className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-[#ffc107] size-20">
           <ActivityIndicator size="large" color="#ffc107" />
         </View>
       )}
@@ -15,8 +16,10 @@ const App = () => {
         source={{ uri: 'https://skilfulonline.site/' }} // Replace with your website URL
         className='flex-1'
         onLoadEnd={() => setIsLoading(false)}
-        startInLoadingState={true}
+        // startInLoadingState={true}
+        scalesPageToFit={false} // Prevent zoom
         javaScriptEnabled={true}
+        
       />
     </View>
   );
