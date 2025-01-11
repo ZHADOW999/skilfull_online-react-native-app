30d0655e1b1b8a79845b6a9a67ad6cc04ccd5023
ZHADOW999 <babalolav9@gmail.com>
2025-01-11 12:43:52 +0100

    Refactor project structure and update dependencies
    - Renamed project from "skilfull_online" to "skilful_online" in app.json and package.json.
    - Updated splash screen configuration in app.json.
    - Added new dependencies: nativewind and tailwindcss.
    - Removed unused components and files, including NotFoundScreen, TabLayout, and various UI components.
    - Simplified RootLayout to use a basic Stack without theming.
    - Updated TypeScript configuration to include nativewind-env.d.ts.
    - Updated images and removed unused assets.
    - Cleaned up package-lock.json to reflect new dependencies.
    
    This commit streamlines the project and prepares it for further development.

---
+++ app/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { ActivityIndicator, StyleSheet, View } from 'react-native';
+import { WebView } from 'react-native-webview';
+
+const App = () => {
+  const [isLoading, setIsLoading] = React.useState(true);
+  return (
+    <View className='flex-1'>
+      {isLoading && (
+        <View className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-[#ffc107]">
+          <ActivityIndicator size="large" color="#ffc107" />
+        </View>
+      )}
+      <WebView 
+        source={{ uri: 'https://skilfulonline.site/' }} // Replace with your website URL
+        className='flex-1'
+        onLoadEnd={() => setIsLoading(false)}
+        startInLoadingState={true}
+        javaScriptEnabled={true}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  webview: {
+    flex: 1,
+  },
+});
+
+export default App;
